714b4e3a99ed7d1b10ee4ef1a081cf1c37173831
nickbarth <barth.nicholas@gmail.com>
2018-08-15 19:39:11 -0400

    did the bonus

--- src/components/map/maprenderer/index.tsx
+++ src/components/map/maprenderer/index.tsx
@@ -1,15 +1,20 @@
 
 // Vendors
 import * as React from 'react';
+import { connect } from 'react-redux';
 
-import { GoogleMap, DirectionsRenderer } from 'react-google-maps';
+import { GoogleMap, DirectionsRenderer, Marker } from 'react-google-maps';
 
 interface IProps {
 	origin?: any,
-	destination?: any
+	destination?: any,
+	user: {
+		user: any,
+		company: any
+	}
 };
 
-export default class MapRenderer extends React.Component<IProps, any> {
+class MapRenderer extends React.Component<IProps, any> {
 
 	constructor(props:any) {
 		super(props);
@@ -17,6 +22,22 @@ export default class MapRenderer extends React.Component<IProps, any> {
 		this.state = {
 			directions: null
 		};
+
+		console.log(props.user.company);
+
+		const googleapiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+		const formattedAddress = props.user.company.billing_address.formatted_address.replace(/ /g, '+');
+		const googkeApiKey = '&key=AIzaSyCHoslqFGMAt5Lz1DL4NjjEEVBONNVqgJo';
+
+		fetch(googleapiUrl + formattedAddress + googkeApiKey, {
+			method: 'get',
+		}).then((resp:any) => {
+			return resp.json()
+		}).then((data) => {
+			this.setState({ marker: {lat: data.results[0].geometry.location.lat, lon: data.results[0].geometry.location.lng}});
+		}).catch((error) => {
+			console.log('dude there is an', error);
+		});
 
 		const DirectionsService = new google.maps.DirectionsService();
 
@@ -40,8 +61,9 @@ export default class MapRenderer extends React.Component<IProps, any> {
 	}
 
 	render () {
-		const { directions } = this.state;
-		const { origin } = this.props;
+		const { directions, marker } = this.state;
+		const { origin, user } = this.props;
+
 
 		return (
 			<React.Fragment>
@@ -51,9 +73,27 @@ export default class MapRenderer extends React.Component<IProps, any> {
 						defaultCenter={new google.maps.LatLng(origin.lat, origin.lan)}
 					>
 						<DirectionsRenderer directions={directions} />
+						<Marker
+							position={{ lat: marker.lat, lng: marker.lon }}
+						/>
 					</GoogleMap>
 				) : null}
+			<div className="map__redux">
+				Behold the amazing power of a redux store!
+				<div>
+					Username: {user.user.first_name} {user.user.last_name}
+				</div>
+				<div>
+					Company: {user.company.name}
+				</div>
+			</div>
 			</React.Fragment>
 		)
 	}
 }
+
+const mapStateToProps = (state: any) => ({
+  user: state.user.user
+});
+
+export default connect(mapStateToProps, null)(MapRenderer);
